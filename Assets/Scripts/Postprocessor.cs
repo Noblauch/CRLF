55e3baf0d33ba220d0603b39bbfeaaaf09482521
Noblauch <n.block@whow.net>
2022-01-06 10:14:32 +0100

    added Postprocessor

---
+++ Assets/Scripts/Postprocessor.cs
@@ -0,0 +1,19 @@
+using UnityEditor;
+using UnityEngine;
+
+public class Postprocessor : AssetPostprocessor
+{
+    static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets,
+        string[] movedFromAssetPaths)
+    {
+        foreach (string str in importedAssets)
+            if (str.EndsWith(".prefab"))
+                PostprocessPrefab(str);
+    }
+    
+    static void PostprocessPrefab(string prefabPath)
+    {
+        var prefabRoot = AssetDatabase.LoadMainAssetAtPath(prefabPath) as GameObject;
+        PrefabUtility.SavePrefabAsset(prefabRoot);
+    }
+}
